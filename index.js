17f2bf6c337908599c7ca9686a30f204364148c4
OsiaruRobert <osiarurobert@gmail.com>
2025-03-23 04:51:14 +0100

    Its working

--- index.js
+++ index.js
@@ -10,22 +10,11 @@ const
 // Express initiation
 const app = express();
 
-// log every request to the terminal
-app.use((req, res, next) => {
-  console.log(`You are going to :${req}`);
-  console.log(`You are getting to :${req}`);
-  next();
-});
 
 // home page route
 app.get('/', (req, res) => {
   res.send('Hello World! version 1.0.1');
 });
-
-// serve static assets
-app.use(express.static( 'static' ));
-
-
 
 
 // start server
